139e8857a00bf2c88ba9a67ac6c05c9dabb85299
unknown <zakimaulana0754@gmail.com>
2025-11-29 22:59:32 +0700

    update

--- src/pages/Voting.jsx
+++ src/pages/Voting.jsx
@@ -85,9 +85,7 @@ export default function Voting() {
   // ===================== CEK REAL-TIME ADMIN STOP =====================
   useEffect(() => {
     const interval = setInterval(async () => {
-      const res = await fetch(
-        `${import.meta.env.VITE_API_URL}/api/voting/status`
-      );
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/voting/status`);
       const { voting_open } = await res.json();
 
       setVotingOpen(voting_open);
